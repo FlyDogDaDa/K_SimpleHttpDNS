f6d7c623e725dcf0bb569ebb87b6857ac69bb673
飛天柴犬 <71758353+FlyDogDaDa@users.noreply.github.com>
2024-12-05 10:23:24 +0800

    Update setup.py
    更新版本號碼

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="k_simple_http_dns",
-    version="0.1.0",
+    version="0.1.1",
     description="The local network is scattered with many small ai services, and this package helps manage these hosts and ports.",
     author="HSU, MING-SHUN",
     author_email="54river0522@gmail.com",
